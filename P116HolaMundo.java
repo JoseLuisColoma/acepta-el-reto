3023c6f10b64c05945fb2991680f3ce6384d5638
Jose Luis Coloma <colomatormojl@gmail.com>
2022-01-06 14:31:13 +0100

    cambio P116 v1

--- P116HolaMundo.java
+++ P116HolaMundo.java
@@ -11,7 +11,7 @@ public class P116HolaMundo {
         int n = Integer.parseInt(in.readLine());
 
         if (n >= 0 && n <= 5) {
-            for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
 
                 System.out.println("Hola mundo.");
             }
